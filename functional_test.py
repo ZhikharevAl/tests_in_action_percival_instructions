8e3626a8e99fa929c483dd952af90ed7b20b11e6
AlexeyZh <waltafunk@gmail.com>
2023-10-28 19:05:14 +0300

    refactor

--- functional_test.py
+++ functional_test.py
@@ -1,10 +1,10 @@
 
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 
-def test_has_title(page: Page):
+def test_has_content(page: Page):
     page.goto("http://localhost:8000/")
 
     content = page.inner_text("body > pre")
 
-    assert "FastAPI" in content
+    assert "FastAPI" in content, f"{content} should contain 'FastAPI'"
